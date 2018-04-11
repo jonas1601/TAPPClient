63735680e15de9ffb3411beb42371b756559d860
melukas <l.kiffmeyer@lvm.de>
2018-04-11 08:37:45 +0200

    Änderung Auswahl Gruppen

--- src/pages/termine/termine.ts
+++ src/pages/termine/termine.ts
@@ -13,36 +13,6 @@ export class TerminePage {
   constructor(public navCtrl: NavController, public navParams: NavParams, public alertCtrl: AlertController) {
   }
 
-  showCheckbox(){
-    let alert = this.alertCtrl.create();
-    alert.setTitle('Welche Termine möchten sie anzeigen?');
-
-    alert.addInput({
-      type: 'checkbox',
-      label: 'Warendorfer Sportunion',
-      value: 'value1',
-      checked: false
-    });
-
-    alert.addInput({
-      type: 'checkbox',
-      label: 'Orchesterverein Freckenhorst',
-      value: 'value1',
-      checked: true
-    });
-
-    alert.addButton('Cancel');
-    alert.addButton({
-      text: 'Okay',
-      handler: data => {
-        console.log('Checkbox data:', data);
-        //this.testCheckboxOpen = false;
-        //this.testCheckboxResult = data;
-      }
-    });
-    alert.present();
-
-  }
 
   
 
